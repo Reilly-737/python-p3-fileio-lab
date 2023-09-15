3d7c50b0880e88b0b9b64512000eb8d86fa2a8bc
+ <+>
2023-09-14 18:49:31 -0600

    finished lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
 def write_file(file_name, file_content):
-    pass
+    # Convert file_name to a string and add the ".txt" extension
+    file_name = str(file_name) + ".txt"
+    
+    # Open the file in write mode and write the content
+    with open(file_name, "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    # Convert file_name to a string and add the ".txt" extension
+    file_name = str(file_name) + ".txt"
+    
+    # Open the file in append mode and append the content
+    with open(file_name, "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    # Convert file_name to a string and add the ".txt" extension
+    file_name = str(file_name) + ".txt"
+    
+    # Open the file in read mode and read its content
+    with open(file_name, "r") as file:
+        return file.read()
